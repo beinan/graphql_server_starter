084601b5c663c8ab8af66ce9c443c6846c257eb1
Beinan Wang <beinanw@twitter.com>
2018-01-23 16:28:20 -0800

    fix dependency erros

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 const root_schema = require('./schema/root')
 const logger = require('./utils/logger')
-const app = express();
+const app = express()
+
+const port = 8888
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: root_schema }));
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
-app.listen(4000, () => logger.info('Now browse to localhost:4000/graphiql'));
+
+app.listen(port, () => logger.info('Now browse to http://localhost:' + port + '/graphiql'));
